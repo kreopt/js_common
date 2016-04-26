2e1763d07d13dacf87e93e70a8f5f45316978d87
kreopt_wf34 <kreopt@gmail.com>
2016-04-26 16:44:48 +0600

    * fix typo

--- bp_event/event.js
+++ bp_event/event.js
@@ -1,17 +1,18 @@
+const listener = Symbol("event_listener");
 export class EventEmitter {
     constructor() {
-        this.listener = document.createElement('div');
+        this[listener] = document.createElement('div');
     }
 
     addEventListener(event, fn) {
-        this.listener.addEventListener(event, fn, false);
+        this[listener].addEventListener(event, fn, false);
     }
 
     removeEventListener(event, fn) {
-        this.listener.removeEventListener(event, fn, false);
+        this[listener].removeEventListener(event, fn, false);
     }
 
-    trigger(event, data) {
-        this.listener.dispatchEvent(new CustomEvent(event, {detail: data}))
+    dispatchEvent(event, data) {
+        this[listener].dispatchEvent(new CustomEvent(event, {detail: data}))
     }
 }
