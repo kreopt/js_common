b31d066b8c7b49766adfc7f1591a8b9fe3097d7e
kreopt_wf34 <kreopt@gmail.com>
2016-04-28 11:16:22 +0600

    * update template engine

--- bp_template/template.js
+++ bp_template/template.js
@@ -1,44 +1,29 @@
-function html(literalSections, ...substs) {
-    // Use raw literal sections: we don’t want
-    // backslashes (\n etc.) to be interpreted
-    let raw = literalSections.raw;
-
-    let result = '';
-
-    substs.forEach((subst, i) => {
-        // Retrieve the literal section preceding
-        // the current substitution
-        let lit = raw[i];
-
-        // In the example, map() returns an array:
-        // If substitution is an array (and not a string),
-        // we turn it into a string
-        if (Array.isArray(subst)) {
-            subst = subst.join('');
-        }
-
-        // If the substitution is preceded by a dollar sign,
-        // we escape special characters in it
-        if (lit.endsWith('$')) {
-            subst = htmlEscape(subst);
-            lit = lit.slice(0, -1);
-        }
-        result += lit;
-        result += subst;
-    });
-    // Take care of last literal section
-    // (Never fails, because an empty template string
-    // produces one literal section, an empty string)
-    result += raw[raw.length-1]; // (A)
-
-    return result;
-}
-
 export const Template = {
-    render(id, data) {
-        let t = document.getElementById(id);
+    render:function(template, data) {
+        let t = (typeof template == typeof '') ? document.getElementById(template) : template;
+        if (!(t instanceof HTMLTemplateElement)) {
+            throw new Error('invalid template');
+        }
+
         let clone = document.importNode(t.content, true);
-        // TODO: fill with data
+        let cached = {};
+        for (let selector in data) {
+            let [sel, attr] = selector.split('@');
+            let node = cached[sel];
+            if (!node) {
+                node = clone.querySelector(sel);
+                cached[sel] = node;
+            }
+            if (!attr) {
+                node.innerHTML = data[selector];
+            } else {
+                if (attr.startsWith('#')) {
+                    node.innerHTML.replace(new RegExp(`([^$])\\$\{${attr.substr(1)}\}`, 'g'), `$1${data[selector]}`);
+                } else {
+                    node.setAttribute(attr, data[selector]);
+                }
+            }
+        }
         return clone;
     }
 };
