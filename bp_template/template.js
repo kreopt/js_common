a03b21ba05d09ad1e13299085b88efd272900541
kreopt_wf34 <kreopt@gmail.com>
2016-08-15 09:48:23 +0700

    * update template data format

--- bp_template/template.js
+++ bp_template/template.js
@@ -1,6 +1,27 @@
-export const Template = {
+/*export */const Template = {
     interpolateVar(string, variable, value) {
-        return string.replace(new RegExp(`([^$])\\$\{${variable}\}`, 'g'), `$1${value}`)
+        return string.replace(new RegExp(`([^$])?\\$\{${variable}\}`, 'g'), `$1${value}`)
+    },
+    interpolateChildren(node, variable, value) {
+        for (let i=0; i<node.childNodes.length; ++i) {
+            let child = node.childNodes[i];
+            // Template.interpolateRecursive(child, variable, value);
+            if (child.nodeType == Node.ELEMENT_NODE) {
+                for (let attr of child.attributes) {
+                    child.setAttribute(attr.name, Template.interpolateVar(attr.textContent, variable, value))
+                }
+            }
+
+            switch (child.nodeType) {
+                case Node.ELEMENT_NODE:
+                    child.innerHTML = Template.interpolateVar(child.innerHTML, variable, value);
+                    break;
+                case Node.TEXT_NODE:
+                    child.nodeValue = Template.interpolateVar(child.nodeValue, variable, value);
+                    break;
+                default:;
+            }
+        }
     },
     render:function(template, data) {
         let t = (typeof template == typeof '') ? document.getElementById(template) : template;
@@ -11,42 +32,35 @@ export const Template = {
         let clone = document.importNode(t.content, true);
         let cached = {};
         for (let selector in data) {
-            let [sel, attr] = selector.split('@');
-            let node = cached[sel];
-            if (!node) {
-                if (sel) {
-                    node = clone.querySelector(sel);
-                    cached[sel] = node;
-                }
-            }
-            if (!attr) {
-                node.innerHTML = data[selector];
-            } else {
-                if (attr.startsWith('#')) {
-                    let variable = attr.substr(1);
-                    let value = data[selector];
-                    if (node) {
-                        node.innerHTML = Template.interpolateVar(node.innerHTML, variable, value);
-                    } else {
-                        for (let i=0; i<clone.childNodes.length; ++i) {
-                            let child = clone.childNodes[i];
-                            switch (child.nodeType) {
-                                case Node.ELEMENT_NODE:
-                                    child.innerHTML = Template.interpolateVar(child.innerHTML, variable, value);
-                                    break;
-                                case Node.TEXT_NODE:
-                                    child.nodeValue = Template.interpolateVar(child.nodeValue, variable, value);
-                                    break;
-                                default:;
-                            }
-                        }
-                        // clone.textContent.replace(new RegExp(`([^$])\\$\{${attr.substr(1)}\}`, 'g'), `$1${data[selector]}`);
+            if (selector.startsWith('=')) {
+                let [sel, attr] = selector.split('@');
+                sel = sel.substr(1);
+                let node = cached[sel];
+                if (!node) {
+                    if (sel) {
+                        node = clone.querySelector(sel);
+                        cached[sel] = node;
                     }
+                }
+
+                if (!attr) {
+                    node.innerHTML = data[selector];
                 } else {
                     node.setAttribute(attr, data[selector]);
                 }
+            } else {
+                Template.interpolateChildren(clone, selector, data[selector]);
             }
         }
         return clone;
     }
 };
+/**
+
+ Template.render(template_id, {
+    'variable': 'string value',
+    '=.selector': 'innerHTML setter',
+    '=.selector@attribute': 'attribute setter'
+ })
+
+ */
